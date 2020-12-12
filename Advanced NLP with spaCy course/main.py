f25c6c7a7a8b0715a88447e4e5b0ccad1ab17c19
ShaunakSen <shaunak1105@gmail.com>
2020-12-12 13:14:56 +0530

    basic bar chart

--- Advanced NLP with spaCy course/main.py
+++ Advanced NLP with spaCy course/main.py
@@ -1,6 +1,12 @@
 import enum
 import nltk
 from nltk.util import pr
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import string
+import re
+import pandas as pd
+import plotly.express as px
 
 
 def read_file():
@@ -19,7 +25,7 @@ def read_file():
     ### some words like 
 
 
-def normalize_text(text, steps = ['tokenize', 'whitespace_removal', 'case_normal', 'hyphen_handling', 'punc_removal']):
+def normalize_text(text, steps = ['tokenize', 'whitespace_removal', 'case_normal', 'hyphen_handling', 'punc_removal', 'stem']):
   """
   normalize the text according to the pipeline specified
   """
@@ -48,9 +54,45 @@ def normalize_text(text, steps = ['tokenize', 'whitespace_removal', 'case_normal
       words_hyphen_corrected.append(word)
 
 
-  print (words_hyphen_corrected)
-  
-  print (nltk.corpus.stopwords.words('english'))
+  punc_removed = [re.sub(r'[^\w\d]', ' ', word) if len(word)==1 else word for word in words_hyphen_corrected]
+
+
+  stop_words = set(stopwords.words('english'))
+  stopwords_removed = [w for w in punc_removed if not w in stop_words]
+
+
+  ### stemming: while analyzing frequency it will be useful, for example (laughing, laugh, laughed) are all forms of 'laugh' and counted together 
+  # porter = PorterStemmer()
+  # stemmed_words = [porter.stem(word) for word in stopwords_removed]
+
+  # print (stemmed_words)
+
+  return punc_removed, stopwords_removed
+
+
+def analyze_frequency(punc_removed, stopwords_removed):
+  """
+  Now that we have normalized the text we can analyze the frequecy of the words
+  """
+
+  freq_dict = {}
+  for word in stopwords_removed:
+    if word == ' ':
+      continue
+    if word in freq_dict:
+      freq_dict[word] +=1
+    else:
+      freq_dict[word] = 1
+    
+  print (freq_dict)
+
+  df_data = {'word': list(freq_dict.keys()), 'frequency': list(freq_dict.values())}
+
+  df = pd.DataFrame(df_data).sort_values(by='frequency', ascending=False)
+  print (df)
+
+  fig = px.bar(df, x='word', y='frequency')
+  fig.show()
 
 fpath = 'C:\\Users\\shaun\\Documents\\my_projects\\Natural Language Processing\\Advanced NLP with spaCy course'
 
@@ -58,5 +100,23 @@ def main():
 
   ### Step1 : read the file
   text = read_file()
-  normalize_text(text)
+  punc_removed, stopwords_removed = normalize_text(text)
+  analyze_frequency(punc_removed, stopwords_removed)
+
 main()
+
+
+"""
+WordCloud: https://github.com/amueller/word_cloud
+
+CountVectorizer, TFIDF, Word2vec
+
+each word -> feature
+each row: speech by MORPHEUS or NEO
+other simple features like num_stopwords, sentiment 
+Train supervised models
+
+doc2vec: Speaker A -> doc -> vec
+new speech -> embed -> compute dist -> whichever is lesser
+
+"""
